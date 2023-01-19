bc64a8855d22c446cf34c3916b717069601c6dbc
Rodrigo Aguiar Silva <86214343+RodrigoAguiarS@users.noreply.github.com>
2023-01-19 19:02:57 -0300

    primeiro commit consumindo api do imdb

---
+++ src/main/java/br/com/rodrigo/api/ApiDeFilmesImdbApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeFilmesImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeFilmesImdbApplication.class, args);
+	}
+
+}
